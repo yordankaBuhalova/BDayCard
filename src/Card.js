ce08bd7887c362c65150ebb72775af4d3f6d83fe
Yordanka Buhalova <danibuhalova@gmail.com>
2020-01-17 14:23:37 +0100

    Add new font

--- src/Card.js
+++ src/Card.js
@@ -1,19 +1,21 @@
 import React from 'react';
-
 import './App.css';
 import Container from 'react-bootstrap/Container'
 import { TiArrowBack } from "react-icons/ti";
 import Nav from 'react-bootstrap/Nav'
+
+
 function App() {
   return (
     <Container className="App">
-      <div className="App-header cardstyle">
+      <div className="App-header cardstyle card">
           <div>
-        <p style={{ color: 'black'}} >Честит Рожден Ден!</p>
+        <p style={{ color: 'black'}} >Happy Birthday!</p>
         <p style={{ color: 'black'}}>
-            Радост много, усмивки още повече,<br></br> успехи безчет и чудесно здраве! Обичаме те!<br></br> Наздраве!
+        Joy a lot, smiles even more,<br></br>
+        good luck and great health! We love you!<br></br>
             </p>
-            <p style={{ color: 'black'}}>От .....</p>
+            <p style={{ color: 'black'}}>From .....</p>
             </div>
         <Nav className="justify-content-end" activeKey="/home">
             <Nav.Link  style={{ textDecoration: 'none', color: 'black'}} className="backbtn" href="/home"><TiArrowBack/> </Nav.Link>
